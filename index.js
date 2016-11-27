885dc2d7e3cefb29fa0c2d89695ddfed7ccf4802
Kevin Salter <kevins@7Geese.com>
2016-11-27 15:59:12 -0800

    testing

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import 'rxjs/add/observable/interval'
 import 'rxjs/add/operator/take'
 
 
-Observable.interval(1000).take(5).subscribe(x => console.log(x))
+Observable.interval(1000).take(10).subscribe(x => console.log(x))
 document.getElementById('root').textContent = 'i\'m alive'
