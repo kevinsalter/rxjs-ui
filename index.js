2cca38b891e98f41c6b4d8c2a7be53f42ddf5d0a
Kevin Salter <kevins@7Geese.com>
2016-11-27 15:39:00 -0800

    add surge.sh, git-hooks, and a webpack build command

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import 'rxjs/add/observable/interval'
 import 'rxjs/add/operator/take'
 
 
-const stopwatch = Observable.interval(1000).take(5).subscribe(x => console.log(x))
+Observable.interval(1000).take(5).subscribe(x => console.log(x))
+document.getElementById('root').textContent = 'i\'m alive'
