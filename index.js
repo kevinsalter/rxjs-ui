1463c2f37b9a86e3a187766c8e6e578cdd9d67df
Kevin Salter <kevins@7Geese.com>
2016-11-28 00:15:24 -0800

    add some CSS and basic imperative JS

--- index.js
+++ index.js
@@ -2,5 +2,24 @@ const videoPlayer = document.getElementById('coffee-video')
 const playButton = document.getElementById('play')
 const pauseButton = document.getElementById('pause')
 
+videoPlayer.addEventListener('mouseenter', () => {
+    playButton.classList.add('visible')
+    pauseButton.classList.add('visible')
+})
+
+videoPlayer.addEventListener('mouseleave', () => {
+    playButton.classList.remove('visible')
+    pauseButton.classList.remove('visible')
+})
+
+playButton.addEventListener('mousemove', () => {
+    playButton.classList.add('visible')
+    pauseButton.classList.add('visible')
+})
+pauseButton.addEventListener('mousemove', () => {
+    playButton.classList.add('visible')
+    pauseButton.classList.add('visible')
+})
+
 playButton.addEventListener('click', () => videoPlayer.play())
 pauseButton.addEventListener('click', () => videoPlayer.pause())
