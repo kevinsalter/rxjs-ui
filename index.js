0e46996cdd89fc79de56d2433b8672f172caa322
Kevin Salter <kevins@7Geese.com>
2016-11-27 22:36:30 -0800

    update some eslintrc stuff

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const videoPlayer = document.getElementById('coffee-video')
 const playButton = document.getElementById('play')
 const pauseButton = document.getElementById('pause')
 
-playButton.addEventListener('click', () => videoPlayer.play());
-pauseButton.addEventListener('click', () => videoPlayer.pause());
+playButton.addEventListener('click', () => videoPlayer.play())
+pauseButton.addEventListener('click', () => videoPlayer.pause())
