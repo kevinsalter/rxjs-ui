49e711a8d0f4696042d6f2a00a460a4340c0f430
Kevin Salter <kevins@7Geese.com>
2016-11-28 09:30:11 -0800

    refactor visible class adding/removing a bit

--- index.js
+++ index.js
@@ -2,24 +2,21 @@ const videoPlayer = document.getElementById('coffee-video')
 const playButton = document.getElementById('play')
 const pauseButton = document.getElementById('pause')
 
-videoPlayer.addEventListener('mouseenter', () => {
+const addVisibleClass = () => {
     playButton.classList.add('visible')
     pauseButton.classList.add('visible')
-})
+}
 
-videoPlayer.addEventListener('mouseleave', () => {
+const removeVisibleClass = () => {
     playButton.classList.remove('visible')
     pauseButton.classList.remove('visible')
-})
+}
 
-playButton.addEventListener('mousemove', () => {
-    playButton.classList.add('visible')
-    pauseButton.classList.add('visible')
-})
-pauseButton.addEventListener('mousemove', () => {
-    playButton.classList.add('visible')
-    pauseButton.classList.add('visible')
-})
+videoPlayer.addEventListener('mouseenter', addVisibleClass)
+videoPlayer.addEventListener('mouseleave', removeVisibleClass)
+
+playButton.addEventListener('mousemove', addVisibleClass)
+pauseButton.addEventListener('mousemove', addVisibleClass)
 
 playButton.addEventListener('click', () => videoPlayer.play())
 pauseButton.addEventListener('click', () => videoPlayer.pause())
